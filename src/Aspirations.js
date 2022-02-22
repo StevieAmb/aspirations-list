2571b9092a76c38161f7cde34d641212127aebb4
Stevie Ambroise <s.ambroise@protonmail.com>
2022-02-21 19:19:00 -0700

    Begin tester boilerplate for aspirations list

---
+++ src/Aspirations.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Aspirations = () => {
+  return (
+    <h4>Write a book by age 50</h4>
+  )
+}
+
+
+export default Aspirations;
---
+++ src/AspirationsList.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Aspirations from "./Aspirations";
+
+const AspirationsList = () => {
+  return (
+    <div>
+      <h4>My aspirations are...</h4>
+      <Aspirations />
+    </div>
+  )
+}
+
+export default AspirationsList;
